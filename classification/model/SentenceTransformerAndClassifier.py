7cc4d6cbb82eae34909dd10337e04ed496ba4c6c
Maksim Moiseikin <m.moiseikin@gmail.com>
2021-07-10 14:29:50 +0200

    Define a single model for encoding and classification

---
+++ classification/model/SentenceTransformerAndClassifier.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+
+import torch
+from prettytable import PrettyTable
+from torch import nn, Tensor
+from transformers import AutoModel
+from transformers.modeling_outputs import BaseModelOutputWithPooling
+
+from classification.model.utils import mean_pooling
+
+
+@dataclass
+class SentenceTransformerAndClassifierResult:
+    token_embeddings: Tensor
+    sentence_embeddings: Tensor
+    classification_result: Tensor
+
+
+class SentenceTransformerAndClassifier(nn.Module):
+
+    def __init__(self, sentence_transformer_model: str, n_classes: int, dropout_rate: float = 0.5, embedding_dim=768,
+                 hidden_dim=512):
+        super().__init__()
+        self.sentence_transformer = AutoModel.from_pretrained(sentence_transformer_model)
+
+        # freeze parameters of sentence_transformer - we want to train only the classifier part
+        for name, param in self.sentence_transformer.named_parameters():
+            param.requires_grad = False
+
+        self.linear_layer = nn.Linear(embedding_dim, hidden_dim)
+        self.activation = nn.LeakyReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.classifier = nn.Linear(hidden_dim, n_classes)
+
+    def count_parameters(self, skip_frozen=True):
+        table = PrettyTable(["Modules", "Parameters"])
+        total_params = 0
+        for name, parameter in self.named_parameters():
+            if skip_frozen and not parameter.requires_grad:
+                continue
+            param = parameter.numel()
+            table.add_row([name, param])
+            total_params += param
+        print(table)
+        if skip_frozen:
+            print(f"Total trainable parameters: {total_params}")
+        else:
+            print(f"Total parameters: {total_params}")
+        return total_params
+
+    def compute_token_embeddings(self, input_ids, attention_mask):
+        sentence_transformer_output: BaseModelOutputWithPooling = self.sentence_transformer(input_ids, attention_mask)
+        token_embeddings: Tensor = sentence_transformer_output["last_hidden_state"]
+        return token_embeddings
+
+    def _get_logits(self, token_embeddings):
+        linear_layer_output = self.linear_layer(token_embeddings[:, 0])
+        linear_layer_output = self.activation(linear_layer_output)
+
+        dropout_output = self.dropout(linear_layer_output)
+
+        output = self.classifier(dropout_output)
+        return output
+
+    def forward(self, input_ids, attention_mask):
+        token_embeddings = self.compute_token_embeddings(input_ids, attention_mask)
+        output = self._get_logits(token_embeddings)
+        return output
+
+    def classify(self, token_embeddings):
+        classification_result = self._get_logits(token_embeddings)
+        return torch.nn.Softmax(dim=1)(classification_result)
+
+    def encode_and_classify(self, **encoded_input) -> SentenceTransformerAndClassifierResult:
+        self.eval()
+        with torch.no_grad():
+            token_embeddings: Tensor = self.compute_token_embeddings(**encoded_input)
+            sentence_embeddings = mean_pooling(token_embeddings, encoded_input["attention_mask"])
+            classification_result = self.classify(token_embeddings)
+
+        return SentenceTransformerAndClassifierResult(token_embeddings, sentence_embeddings, classification_result)
--- clustering/clustering.py
+++ clustering/clustering.py
@@ -2,9 +2,12 @@ import logging
 from typing import List
 
 import torch
-from sentence_transformers import util, SentenceTransformer
+from sentence_transformers import util
 from torch import Tensor
+from transformers import AutoTokenizer
 
+from classification.model.SentenceTransformerAndClassifier import SentenceTransformerAndClassifier, \
+    SentenceTransformerAndClassifierResult
 from entity import Entity
 
 logger = logging.getLogger(__name__)
@@ -19,15 +22,13 @@ THRESHOLD = 0.85
 
 class EntityClustering(object):
 
-    def __init__(self, entity_encoder: SentenceTransformer):
-        self.entity_encoder: SentenceTransformer = entity_encoder
+    def __init__(self):
         self.n_clusters = 0
         self.entities: List[Entity] = []
 
-    def process(self, input_string: str):
-        input_embeddings: Tensor = self.entity_encoder.encode(input_string)
+    def process(self, input_string: str, sentence_embeddings: Tensor):
         for entity in self.entities:
-            cosine_similarity = util.pytorch_cos_sim(input_embeddings, entity.representative)
+            cosine_similarity = util.pytorch_cos_sim(sentence_embeddings, entity.representative)
             cosine_similarity = cosine_similarity.cpu().detach().numpy()[0][0]
 
             if cosine_similarity > THRESHOLD:
@@ -41,18 +42,21 @@ class EntityClustering(object):
         self.n_clusters += 1
 
         entity = Entity()
-        entity.representative = input_embeddings
+        entity.representative = sentence_embeddings
         entity.synonyms.append(input_string)
         self.entities.append(entity)
 
 
 if __name__ == "__main__":
     logger.setLevel(logging.INFO)
-    logger.debug("CUDA avaiable: " + str(torch.cuda.is_available()))
+    logger.debug("CUDA available: " + str(torch.cuda.is_available()))
 
-    model = SentenceTransformer("sentence-transformers/paraphrase-mpnet-base-v2")
-    entityClustering = EntityClustering(model)
+    model = SentenceTransformerAndClassifier("sentence-transformers/paraphrase-mpnet-base-v2", n_classes=5)
+    tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/paraphrase-mpnet-base-v2")
+    entityClustering = EntityClustering()
 
     while True:
         user_input = input("Enter next entity: ")
-        entityClustering.process(user_input)
+        encoded_input = tokenizer(user_input, padding=True, truncation=True, return_tensors='pt')
+        model_output: SentenceTransformerAndClassifierResult = model.encode_and_classify(**encoded_input)
+        entityClustering.process(user_input, model_output.sentence_embeddings)
